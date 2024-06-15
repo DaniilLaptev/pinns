279f79926800d5141a7c63d4a5f6fa9e6c558d92
Daniil Laptev <daniillaptev@outlook.com>
2024-06-15 12:42:03 +0300

    Added relative L2 error.

--- pinns/errors.py
+++ pinns/errors.py
@@ -11,15 +11,21 @@ def l2_error(predicts, target):
         target = target.detach().cpu().numpy()
         
     if predicts.shape != target.shape:
-        try:
-            predicts = predicts.unsqueeze(1)
-        except:
-            raise RuntimeError('Shapes of predicts and targets are different.')
+        raise RuntimeError('Shapes of predicts and targets are different.')
         
     return np.sqrt(np.square(predicts - target).sum())
 
-def rel_l2_error():
-    pass
+def rel_l2_error(predicts, target):
+    
+    if isinstance(predicts, Tensor):
+        predicts = predicts.detach().cpu().numpy()
+    if isinstance(target, Tensor):
+        target = target.detach().cpu().numpy()
+        
+    if predicts.shape != target.shape:
+        raise RuntimeError('Shapes of predicts and targets are different.')
+    
+    return np.linalg.norm(predicts - target, 2) / np.linalg.norm(target, 2)
 
 def rmse_error(predicts, target):
     
